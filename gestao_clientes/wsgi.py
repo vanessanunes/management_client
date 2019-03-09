4ef11c81ac149722b8131cb31ea6c85d8984a27f
Vanessa <vanessa.nunez.p@gmail.com>
2019-03-09 16:56:06 -0300

    rename app gunicorn wsgi

--- gestao_clientes/wsgi.py
+++ gestao_clientes/wsgi.py
@@ -14,4 +14,3 @@ from dj_static import Cling
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestao_clientes.settings')
 
 application = Cling(get_wsgi_application())
-# application = get_wsgi_application()
