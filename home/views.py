8c7d8912cf10a4be02b2ab87c315daf088db6d1d
Vanessa <vanessa.nunez.p@gmail.com>
2019-03-09 14:22:31 -0300

    add home views

--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
 
 def home(request):
     return render(request, 'home.html')
 
-def logout(request):
-    request.user.logout()
+
+def my_logout(request):
+    # request.user.logout()
+    logout(request)
     return redirect('home')
