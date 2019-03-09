2824a2918b0553cba20f72a0a84b395a648430b2
Vanessa <vanessa.nunez.p@gmail.com>
2019-03-09 17:26:32 -0300

    add localhost to allowed hosts settings

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['gestaodeclientesporvanessa.herokuapp.com']
+ALLOWED_HOSTS = ['gestaodeclientesporvanessa.herokuapp.com', 'localhost']
 
 
 # Application definition
