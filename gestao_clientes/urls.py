3f23f0fe6b0356069001a2ec657b38a08d4fb820
Vanessa <vanessa.nunez.p@gmail.com>
2019-03-09 14:33:28 -0300

    create new views to gestao_clientes

--- gestao_clientes/urls.py
+++ gestao_clientes/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('', include(home_urls)),
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('person/', include(clients_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gestao_clientes/views.py
+++ gestao_clientes/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'index.html')
+    pass
+    # return render(request, 'index.html')
